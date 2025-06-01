deba97043afe79de92147696d113581c0d5df2cb
anuruththan <anuruththan1320@gmail.com>
2025-06-01 16:36:15 +0530

    Final update

--- ml_service/random_data/check_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('contributions.csv')
-
-filtered_df = df[df['Name'] == 'Person5']
-filtered_df = filtered_df[filtered_df['CreatedDate'] == '2023-01-03']
-if not filtered_df.empty:
-    print(filtered_df['Contribution'].sum())
